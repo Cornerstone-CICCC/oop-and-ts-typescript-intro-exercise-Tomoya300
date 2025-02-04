6c1d36f250a9725ed97f90d445b4f896fd4bf24a
Tomoya <flyingfox193807@gmail.com>
2025-02-04 13:11:24 -0800

    finished

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,13 +1,12 @@
+"use strict";
 // 3. Create a function named `celsiusToFahrenheit` that takes a number (temperature in Celsius).
 //    The function should return the equivalent temperature in Fahrenheit.
 //    Formula: Fahrenheit = (Celsius * 9/5) + 32
 //    Ensure the parameter and return type are typed correctly.
-
-function celsiusToFahrenheit(celsius: number): number {
-    const newTemp = (celsius*9/5) + 32
-    return newTemp
+function celsiusToFahrenheit(celsius) {
+    const newTemp = (celsius * 9 / 5) + 32;
+    return newTemp;
 }
-
 // Expected output:
-console.log(celsiusToFahrenheit(0));  // 32
+console.log(celsiusToFahrenheit(0)); // 32
 console.log(celsiusToFahrenheit(25)); // 77
--- dist/ex05.js
+++ dist/ex05.js
@@ -1,18 +1,17 @@
+"use strict";
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function countVowels(word: string): number {
-    const splitWord = word.toLowerCase().trim().split('')
-    let result = 0
-    splitWord.forEach((value: string) => {
+function countVowels(word) {
+    const splitWord = word.toLowerCase().trim().split('');
+    let result = 0;
+    splitWord.forEach((value) => {
         if (value === 'a' || value === 'e' || value === 'i' || value === 'o' || value === 'u') {
-            result += 1
+            result += 1;
         }
-    })
-    return result
+    });
+    return result;
 }
-
 // Expected output:
-console.log(countVowels("hello"));    // 2
-console.log(countVowels("typescript"));// 2
+console.log(countVowels("hello")); // 2
+console.log(countVowels("typescript")); // 2
